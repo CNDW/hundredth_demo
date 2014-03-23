06dee1e300a6552522a2c54f74b40dd6f16d8b3d
Travis Peterson <tekman82@yahoo.com>
2014-03-23 13:40:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HundredthDemo::Application.config.secret_key_base = 'b0bcecf043afff8e07a18332291b991eb6b50d93d1df543d840cc67cd70749d3c62e508e7101d498db63f6669aa2b1c575ee5723fdb1020f1f3aa19ecdefb8d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HundredthDemo::Application.config.session_store :cookie_store, key: '_hundredth_demo_session'
